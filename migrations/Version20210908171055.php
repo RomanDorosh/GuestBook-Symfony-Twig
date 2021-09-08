410adc77c9c154b95baefdc12d0292eced3334a5
RomanDorosh <roma.dorosh@gmail.com>
2021-09-08 19:25:47 +0200

    Added slugs for url

--- migrations/Version20210908171055.php
+++ migrations/Version20210908171055.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210901200656 extends AbstractMigration
+final class Version20210908171055 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20210901200656 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE conference CHANGE year year VARCHAR(4) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_911533C8989D9B62 ON conference (slug)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE created_at created_at DATETIME NOT NULL');
-        $this->addSql('ALTER TABLE conference CHANGE year year VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP INDEX UNIQ_911533C8989D9B62 ON conference');
     }
 }
--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Func;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,11 @@ class ConferenceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conference::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy([], ['year' => 'ASC', 'city' => 'ASC']);
     }
 
     // /**
