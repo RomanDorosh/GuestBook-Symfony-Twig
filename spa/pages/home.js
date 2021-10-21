a324a0b7141c73cb3edf43686c19cb59d248bbb2
RomanDorosh <roma.dorosh@gmail.com>
2021-10-21 21:51:50 +0200

    Continue with building SPA

---
+++ spa/pages/home.js
@@ -0,0 +1,11 @@
+import {h} from 'preact';
+
+
+export default function Home() {
+
+    return (
+        <div className="p-3">
+           Home
+        </div>
+    );
+}
--- spa/src/app.js
+++ spa/src/app.js
@@ -1,8 +1,32 @@
+import '../assets/styles/app.scss'
 import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+import Home from '../pages/home';
+import Conference from '../pages/conference';
 
 function App() {
     return (
-        <div>Hello world!</div>
+        <div>
+            <header className="header">
+                <nav className="container">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">
+                            &#128217; Guestbook
+                        </Link>
+                    </div>
+                </nav>
+                <nav className="bg-light border-bottom text-center">
+                    <Link className="nav-conference" href="/conference/madrid-2022">
+                        Madrid 2022
+                    </Link>
+                </nav>
+            </header>
+
+            <Router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </Router>
+       </div>
     )
 }
 
